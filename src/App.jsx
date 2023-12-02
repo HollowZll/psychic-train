6c611b75da3910a2a9947d543df8bd6c1e4c0e4e
HollowZll <118689735+HollowZll@users.noreply.github.com>
2023-12-02 05:12:09 -0600

    Revert "add: general structure for newsletter in mobile"
    This reverts commit a088b242e2a5430216f8bdd8d126e414060da30b.

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,31 @@
 import { useState } from "react";
 import "./App.css";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
-import Mobile from "../images/mobile.svg"
-import List from "../images/icon-list.svg"
+import { Button } from "reactstrap";
 
 function App() {
   const [count, setCount] = useState(0);
-  const buttonStyle = {
-    backgroundColor: 'hsl(234, 29%, 20%)',
-  };
 
   return (
     <>
-    <div className="d-flex justify-content-center flex-column mb-4">
-    <img src={Mobile} alt="Mobile newsletter image" className="imageMobile" />
+     <div>
+      <h1>Hello Reactstrap!</h1>
+      <Button color="primary">Click me</Button>
     </div>
 
-    <div className="d-flex justify-content-left flex-column">
-    <h1>Stay updated</h1>
-    <p>Join 60,000+ product managers receiving monthly updates</p>
-    <div className="d-flex align-item-start">
-      <img src={List} alt="list icons" />
-      <p>Product discovery and building what matters</p>
-    </div>
-    <div className="d-flex align-item-start">
-      <img src={List} alt="list icons" />
-      <p>Measuring to ensure updates are a success</p>
-    </div>
-    <div className="d-flex  align-items-start">
-      <img src={List} alt="list icons" />
-      <p>And much more!</p>
-    </div>
-
-
-    <Form>
-    <FormGroup>
-    <Label for="exampleEmail">
-      Email
-    </Label>
-    <Input
-      id="exampleEmail"
-      name="email"
-      placeholder="email@company.com"
-      type="email"
-    />
-  </FormGroup>
-  
-    <Button
-    className="subscibeButton"
-    style={buttonStyle}
-    size="lg"
-  >
-    Subscribe to monthly newsletter
-  </Button>
-  </Form>
-
-    </div>
+      Stay updated! Join 60,000+ product managers receiving monthly updates on:
+      Product discovery and building what matters Measuring to ensure updates
+      are a success And much more! Email address email@company.com Subscribe to
+      monthly newsletter Sign-up form end Success message start Thanks for
+      subscribing! A confirmation email has been sent to ash@loremcompany.com.
+      Please open it and click the button inside to confirm your subscription.
+      Dismiss message Success message end
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
     </>
   );
 }
